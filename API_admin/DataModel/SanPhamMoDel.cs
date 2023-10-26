e615fccd48ddf78197dde7231e577dec2fbe5295
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-26 20:22:49 +0700

    Add files via upload

---
+++ API_admin/DataModel/SanPhamMoDel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class SanPhamMoDel
+    {
+        public int MaSanPham {  get; set; }
+        public string TenSanPham {  set; get; }
+        public string TheLoai { get; set; }
+        public float Gia {  get; set; }
+        public float GiaGiam {  get; set; }
+        public string TrangThai {  get; set; }
+    }
+}
