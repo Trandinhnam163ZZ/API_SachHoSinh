d7c0dd30ffa39a0c03614ffe966b4edb9cf57d83
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-26 20:24:04 +0700

    Add files via upload

---
+++ API_admin/BusinessLogicLayer/SanPhamBusiness.cs
@@ -0,0 +1,38 @@
+﻿using DataAcessLayer;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+   public class SanPhamBusiness:ISanPhamBusiness
+    {
+        private ISanPhamReposity _res;
+        public SanPhamBusiness(ISanPhamReposity res)
+        {
+            _res = res;
+        }
+        public SanPhamMoDel GetDatabyID(string id)
+        {
+            return _res.GetDatabyID(id);
+        }
+        public SanPhamMoDel GetName(string name)
+        {
+            return GetName(name);
+        }
+        public bool Create(SanPhamMoDel model)
+        {
+            return _res.Create(model);
+        }
+        public bool Update(SanPhamMoDel model)
+        {
+            return _res.Update(model);
+        }
+        public bool Delete(SanPhamMoDel model)
+        { return _res.Delete(model);}
+
+    }
+}
---
+++ API_admin/BusinessLogicLayer/interfaces/ISanPhamBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+   public partial interface  ISanPhamBusiness
+    {
+        SanPhamMoDel GetDatabyID (string id);
+        SanPhamMoDel GetName(string name);
+         bool Create(SanPhamMoDel model);
+         bool Update(SanPhamMoDel model);
+         bool Delete(SanPhamMoDel model);
+    }
+}
