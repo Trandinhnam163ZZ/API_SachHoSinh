cd681b7ca47aa8c47cbbe9f1d1d2c34ab05cbd54
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-26 22:36:52 +0700

    Add files via upload

---
+++ API_admin/Admin/Controller/NhaXBController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLogicLayer;
+using DataModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdMin_APISachHocSinh.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NhaXBController : ControllerBase
+    {
+
+        private INhaXBBusiness _nhaxbBusiness;
+        public NhaXBController(INhaXBBusiness nhaxbBusiness)
+        {
+            _nhaxbBusiness = nhaxbBusiness;
+        }
+        [Route("get-by-id/{id}")]
+
+        [HttpGet]
+        public NhaXBMoDel GetDatabyID(string id)
+        {
+            return _nhaxbBusiness.GetDatabyID(id);
+        }
+
+
+        [Route("get-name /{name}")]
+
+        [HttpGet]
+        public NhaXBMoDel GetName(string name)
+        {
+            return _nhaxbBusiness.GetName(name);
+        }
+
+        [Route("create-nhaxb")]
+        [HttpPost]
+        public NhaXBMoDel CreateItem([FromBody] NhaXBMoDel model)
+        {
+            _nhaxbBusiness.Create(model);
+            return model;
+
+        }
+
+
+        [Route("update-nhaxb")]
+        [HttpPut]
+        public NhaXBMoDel Update([FromBody] NhaXBMoDel model)
+        {
+            _nhaxbBusiness.Update(model);
+            return model;
+
+        }
+
+        [Route("delete-nhaxb")]
+        [HttpDelete]
+        public NhaXBMoDel Delete([FromBody] NhaXBMoDel model)
+        {
+            _nhaxbBusiness.Delete(model);
+            return model;
+
+        }
+
+
+    }
+}
