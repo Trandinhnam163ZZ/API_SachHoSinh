3303b86867a910ac911dce257902a56e4b982aa9
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-27 13:18:31 +0700

    Add files via upload

---
+++ API_admin/DataAcessLayer/HoaDonReposity.cs
@@ -0,0 +1,109 @@
+﻿using DataAccessLayer;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer
+{
+    public class HoaDonReposity:IHoaDonReposity
+    {
+        private IDatabaseHelper _dbHelper;
+
+        public HoaDonReposity(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public HoaDonMoDel GetDatabyID(string id)
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_hoadon_get_by_id",
+                     "@MaHoaDon", id);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<HoaDonMoDel>().FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Create(HoaDonMoDel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_hoadon_create",
+                "@MaHoaDon", model.MaHoaDon,
+                "@NgayTao", model.NgayTao,
+                "@TongGia", model.TongGia,
+                "@TenKhachHang", model.TenKhachHang,
+                 "@DiaChi", model.DiaChi,
+                 "@Email", model.Email,
+                 "@SoDienThoai", model.SoDienThoai,
+                "@DiaChiGiaoHang", model.DiaChiGiaoHang);
+
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+           public bool Update(HoaDonMoDel model)
+       {
+           string msgError = "";
+           try
+           {
+               var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_hoa_don_update",
+               "@MaHoaDon", model.MaHoaDon,
+               "@NgayTao", model.NgayTao,
+               "@TongGia", model.TongGia,
+               "@TenKhachHang", model.TenKhachHang,
+                "@DiaChi", model.DiaChi,
+                "@Email", model.Email,
+                "@SoDienThoai", model.SoDienThoai,
+               "@DiaChiGiaoHang", model.DiaChiGiaoHang,
+               "@list_json_chitiethoadon", model.list_json_chitiethoadon != null ? MessageConvert.SerializeObject(model.list_json_chitiethoadon) : null);
+               if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+               {
+                   throw new Exception(Convert.ToString(result) + msgError);
+               }
+               return true;
+           }
+           catch (Exception ex)
+           {
+               throw ex;
+           }
+       }
+
+        public bool Delete(HoaDonMoDel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_delete_hoadon",
+                "@MaHoaDon", model.MaHoaDon);
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ API_admin/DataAcessLayer/NhaXBReposity.cs
@@ -0,0 +1,118 @@
+﻿using DataAccessLayer;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer
+{
+    public class NhaXBReposity:INhaXBReposity
+    {
+        private IDatabaseHelper _dbHelper;
+
+        public NhaXBReposity(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public NhaXBMoDel GetDatabyID(string id)
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_nhaxb_get_by_id",
+                     "@MaNXB", id);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<NhaXBMoDel>().FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public NhaXBMoDel GetName(string name)
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_nhaxb_getname",
+                     "@TenNXB", name);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<NhaXBMoDel>().FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public bool Create(NhaXBMoDel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_nhaxb_create",
+                "@MaNXB", model.MaNXB,
+                "@TenNXB", model.TenNXB,
+                "@DiaChi", model.DiaChi,
+                "SoDienThoai", model.SoDienThoai);
+
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Update(NhaXBMoDel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_nhaxb_update",
+                "@MaNXB", model.MaNXB,
+                "@TenNXB", model.TenNXB,
+                "@DiaChi", model.DiaChi,
+                "SoDienThoai", model.SoDienThoai);
+
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Delete(NhaXBMoDel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_delete_nhaxb",
+                "@MaNXB", model.MaNXB);
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
