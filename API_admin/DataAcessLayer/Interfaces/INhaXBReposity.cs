82fe43da86ac97e1364af58489a30e09c77bf33a
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-27 13:17:31 +0700

    Add files via upload

---
+++ API_admin/DataAcessLayer/Interfaces/INhaXBReposity.cs
@@ -0,0 +1,18 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer
+{
+    public partial interface INhaXBReposity
+    {
+        NhaXBMoDel GetDatabyID(string id);
+        NhaXBMoDel GetName(string name);
+        bool Create(NhaXBMoDel model);
+        bool Update(NhaXBMoDel model);
+        bool Delete(NhaXBMoDel model);
+    }
+}
