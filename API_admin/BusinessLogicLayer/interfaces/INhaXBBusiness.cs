a4ae2d193b1db165c12ef5e3f1942133f7c4bb08
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-27 13:10:04 +0700

    Add files via upload

---
+++ API_admin/BusinessLogicLayer/interfaces/INhaXBBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public partial interface INhaXBBusiness
+    {
+        NhaXBMoDel GetDatabyID(string id);
+        NhaXBMoDel GetName(string name);
+        bool Create(NhaXBMoDel model);
+        bool Update(NhaXBMoDel model);
+        bool Delete(NhaXBMoDel model);
+    }
+}
