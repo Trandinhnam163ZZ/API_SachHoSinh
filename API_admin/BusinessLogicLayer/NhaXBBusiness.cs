d51bd48883f55784eb64aa981c31b3ff5931a69a
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-27 13:16:26 +0700

    Add files via upload

---
+++ API_admin/BusinessLogicLayer/NhaXBBusiness.cs
@@ -0,0 +1,37 @@
+﻿using DataAcessLayer;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class NhaXBBusiness:INhaXBBusiness
+    {
+        private INhaXBReposity _res;
+        public NhaXBBusiness(INhaXBReposity res)
+        {
+            _res = res;
+        }
+        public NhaXBMoDel GetDatabyID(string id)
+        {
+            return _res.GetDatabyID(id);
+        }
+        public NhaXBMoDel GetName(string name)
+        {
+            return _res.GetName(name);
+        }
+        public bool Create(NhaXBMoDel model)
+        {
+            return _res.Create(model);
+        }
+        public bool Update(NhaXBMoDel model)
+        {
+            return _res.Update(model);
+        }
+        public bool Delete(NhaXBMoDel model)
+        { return _res.Delete(model); }
+    }
+}
