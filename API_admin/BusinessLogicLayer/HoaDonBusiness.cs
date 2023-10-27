e20e53e2d14e6a9d2cb05a0189a3cb53e31d32a0
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-27 13:15:09 +0700

    Add files via upload

---
+++ API_admin/BusinessLogicLayer/HoaDonBusiness.cs
@@ -0,0 +1,39 @@
+﻿using DataAcessLayer;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class HoaDonBusiness:IHoaDonBusiness
+    {
+        private IHoaDonReposity _res;
+        public HoaDonBusiness(IHoaDonReposity res)
+        {
+            _res = res;
+        }
+        public HoaDonMoDel GetDatabyID(string id)
+        {
+            return _res.GetDatabyID(id);
+        }
+
+        public bool Create(HoaDonMoDel model)
+        {
+            return _res.Create(model);
+        }
+
+         public bool Update(HoaDonMoDel model)
+         {
+             return _res.Update(model);
+         }
+
+        public bool Delete(HoaDonMoDel model)
+        {
+            return _res.Delete(model);
+        }
+
+    }
+}
