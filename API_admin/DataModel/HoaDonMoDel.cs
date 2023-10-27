c51f422490c777f1c0fec81b6786a88b51ee7b08
Trandinhnam163ZZ <145883176+Trandinhnam163ZZ@users.noreply.github.com>
2023-10-27 13:19:27 +0700

    Add files via upload

---
+++ API_admin/DataModel/HoaDonMoDel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class HoaDonMoDel
+    {
+        public int MaHoaDon { get; set; }
+        public DateTime NgayTao { get; set; }
+        public float TongGia { get; set; }
+        public string TenKhachHang { get; set; }
+        public string DiaChi { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+        public string DiaChiGiaoHang { get; set; }
+
+        public List<ChiTietHoaDonModel> list_json_chitiethoadon { get; set; }
+    }
+    public class ChiTietHoaDonModel
+    {
+        public int MaChiTietHoaDon { get; set; }
+        public int MaHoaDon { get; set; }
+        public int MaSanPham { get; set; }
+        public int SoLuong { get; set; }
+        public float TongGia { get; set; }
+        public int status { get; set; }
+    }
+}
+
---
+++ API_admin/DataModel/NhaXBMoDel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class NhaXBMoDel
+    {
+        public int MaNXB { get; set; }
+        public string TenNXB { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+    }
+}
